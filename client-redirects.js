7c9edf1397d98af7bb759e06772223a99904fd95
Katherine Enos <katherine.enos@harness.io>
2023-02-21 16:36:41 -0800

    Removed redirects to deleted folders

--- client-redirects.js
+++ client-redirects.js
@@ -100,23 +100,14 @@ module.exports = {
          to: "/docs/platform/Delegates/advanced-installation/install-a-kubernetes-delegate",
        },
     
-    // Created by kat-enos for PR-2421 on Jan 13, 2023
-       {
-         from: "/docs/platform/Delegates/delegate-install-kubernetes/install-harness-delegate-on-kubernetes",
-         to: "/docs/platform/Delegates/install-delegates/kubernetes-delegates/install-harness-delegate-on-kubernetes",
-       },
-
+    
     // Created by kat-enos for PR-738 on Feb 21, 2023
        {
           from: "/docs/platform/Delegates/install-delegates/kubernetes-delegates/install-harness-delegate-on-kubernetes",
           to: "/docs/platform/Delegates/install-delegates/install-a-delegate",
         },
     
-    // Created by kat-enos for PR-2421 on Jan 13, 2023
-       {
-         from: "/docs/platform/Delegates/delegate-install-kubernetes/install-harness-delegate-using-helm",
-         to: "/docs/platform/Delegates/install-delegates/kubernetes-delegates/install-harness-delegate-using-helm",
-       },
+  
 
     // Created by kat-enos for PR-738 on Feb 21, 2023
     {
@@ -124,11 +115,6 @@ module.exports = {
       to: "/docs/platform/Delegates/install-delegates/install-a-delegate",
     },
     
-    // Created by kat-enos for PR-2421 on Jan 13, 2023
-       {
-         from: "/docs/platform/Delegates/delegate-install-docker/install-a-docker-delegate",
-         to: "/docs/platform/Delegates/install-delegates/docker-delegates/install-a-docker-delegate",
-       },
 
     // Created by kat-enos for PR-738 on Feb 21, 2023
     {
